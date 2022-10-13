b0c42bd9adfeff9951937cfb6a439c76b6649747
richard augusto bueno <richard_bueno@ymail.com>
2022-10-13 14:09:29 -0300

    Adicionar arquivos de projeto.

---
+++ Aula02/src/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula02.src.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class HelloController : ControllerBase
+    {
+
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOne([FromRoute] string id)
+        {
+            return Ok($"The id sent was {id}");
+        }
+    }
+}
